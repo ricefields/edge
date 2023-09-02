9b764a78dc5c09c68a762e88950c07db756c9df1
ricefields <ricefields@gmail.com>
2023-09-03 00:09:25 +0530

    commit

--- edge-deploy.py
+++ edge-deploy.py
@@ -119,14 +119,14 @@ st.button(":violet[Start New Edge Conversation]", on_click = reset_engine)
 #st.button("Usage Examples", on_click = usage_examples)
 
 edge_spec = st.text_input ("""Please describe the site-specific changes for your edge node to be applied 
-on top of the base edge configuration listed below. You may specify changes incrementally. 
+on top of the base configuration listed below. You may specify changes incrementally. 
 Each change will apply on the YAML code generated in the previous step and generate the resulting YAML. 
-To start afresh from the base configuration, click on :violet[*Start New Edge Conversation*].
+To start afresh from the base configuration, click on the :violet[*Start New Edge Conversation*] button.
 :violet[**Usage Examples**]: :orange[Change the password of the second master node to 'violin'.]
-:gray[Reduce the number of worker nodes to 2.]
+:grey[Reduce the number of worker nodes to 2.]
 :blue[Change MAC address of the first worker node to 01:02:03:04:05:06 and 
 IP address of its bond0.3803 interface to 172.1.2.3.]
-:rainbow[Increase master nodes to 4 and add section corresponding to the 4th master node.]""", 
+:green[Increase master nodes to 4 and add section corresponding to the 4th master node.]""", 
 key="input")
 
 if edge_spec:
