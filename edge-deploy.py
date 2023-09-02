ba03be4a30de719c8c4f6359e7e03a006936abf0
ricefields <ricefields@gmail.com>
2023-09-03 00:05:10 +0530

    commit# Please enter the commit message for your changes. Lines starting

--- edge-deploy.py
+++ edge-deploy.py
@@ -123,10 +123,10 @@ on top of the base edge configuration listed below. You may specify changes incr
 Each change will apply on the YAML code generated in the previous step and generate the resulting YAML. 
 To start afresh from the base configuration, click on :violet[*Start New Edge Conversation*].
 :violet[**Usage Examples**]: :orange[Change the password of the second master node to 'violin'.]
-:red[Reduce the number of worker nodes to 2.]
+:gray[Reduce the number of worker nodes to 2.]
 :blue[Change MAC address of the first worker node to 01:02:03:04:05:06 and 
 IP address of its bond0.3803 interface to 172.1.2.3.]
-:green[Increase master nodes to 4 and add section corresponding to the 4th master node.]""", 
+:rainbow[Increase master nodes to 4 and add section corresponding to the 4th master node.]""", 
 key="input")
 
 if edge_spec:
