9cf3f49b4bdfe1d3db2cc7e8bf39c0a0b38af52a
ricefields <ricefields@gmail.com>
2023-09-03 15:58:29 +0530

    commit

--- edge-deploy.py
+++ edge-deploy.py
@@ -153,8 +153,9 @@ if edge_spec:
     #edge_spec = "Change the number of master node replicas to 4. Add sections corresponding to any additional master nodes. Keep existing sections unchanged."
 
 if st.session_state['matched_IaC']:
-    st.download_button(':violet[Download]', st.session_state['matched_IaC'], file_name="edge-deploy.yaml")
-    
+    if st.download_button(':violet[Download]', st.session_state['matched_IaC'], file_name="edge-deploy.yaml"):
+        st.stop()
+
     st.write ("Generated YAML:")
     st.code(st.session_state['matched_IaC'], language="yaml", line_numbers=False)   
 
